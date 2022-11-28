9032acaff03075298cd62e093bbd0bb5bb99f60d
Pirashan <pirashan99@gmail.com>
2022-11-27 23:17:37 -0500

    complete build

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+  images: {
+      domains: ["rb.gy", "cdn.sanity.io", "lh3.googleusercontent.com"],
+    // remotePatterns: [
+    //   {
+    //     protocol:'https',
+    //     hostname:'rb.gy',
+    //     port: '',
+    //     pathname: '/vsvv2o',
+    //   },
+    // ],
+  },
+};
